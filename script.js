5858632e46127539346eb8126044ea0272ec91ee
Shiva Dhakal <theskdhakal@gmail.com>
2023-01-24 13:54:03 +1100

    bucket list completed

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const bucketBtn = document.querySelector(".bucket-btn");
 const bucketInput = document.querySelector(".bucket-input");
 const bucketList = document.querySelector(".bucket-list");
 
+document.addEventListener("DOMContentLoaded", getLists());
+
 bucketBtn.addEventListener("click", function addBucket(e) {
   e.preventDefault();
   if (!bucketInput.value) {
@@ -19,6 +21,8 @@ bucketBtn.addEventListener("click", function addBucket(e) {
   newBucket.innerText = bucketInput.value;
 
   bucketDiv.appendChild(newBucket);
+
+  saveLists(bucketInput.value);
 
   //check-btn
   const completeBtn = document.createElement("button");
@@ -54,3 +58,58 @@ bucketList.addEventListener("click", function deleteCheck(e) {
     list.classList.toggle("completed");
   }
 });
+
+// saving the list
+
+function saveLists(list) {
+  let lists;
+
+  if (localStorage.getItem("lists") === null) {
+    lists = [];
+  } else {
+    lists = JSON.parse(localStorage.getItem("lists"));
+  }
+
+  lists.push(list);
+
+  localStorage.setItem("lists", JSON.stringify(lists));
+}
+
+function getLists() {
+  let lists;
+
+  if (localStorage.getItem("lists") === null) {
+    lists = [];
+  } else {
+    lists = JSON.parse(localStorage.getItem("lists"));
+  }
+
+  lists.forEach(function (list) {
+    //   create bucket div
+    const bucketDiv = document.createElement("div");
+    bucketDiv.classList.add("bucket");
+
+    // create li and append to bucket div
+
+    const newBucket = document.createElement("li");
+    newBucket.classList.add("bucket-item");
+    newBucket.innerText = list;
+
+    bucketDiv.appendChild(newBucket);
+
+    //check-btn
+    const completeBtn = document.createElement("button");
+    completeBtn.innerHTML = "<i class='fa-solid fa-check'></i>";
+    completeBtn.classList.add("complete-btn");
+    bucketDiv.appendChild(completeBtn);
+
+    //delete-btn
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "<i class='fa-solid fa-trash'></i>";
+    deleteBtn.classList.add("delete-btn");
+    bucketDiv.appendChild(deleteBtn);
+
+    //   append bucketDiv to bucketList
+    bucketList.appendChild(bucketDiv);
+  });
+}
